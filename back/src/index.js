28469f8ad250563b2ada7136d5ebfe89f3b8a6f3
Ramon Gonzalez <ramongonzalez101@gmail.com>
2025-11-22 23:51:46 -0400

    creacion de bd

---
+++ back/src/index.js
@@ -0,0 +1,32 @@
+import 'dotenv/config'
+import express from 'express'
+import { pgdb } from './config.js'
+import morgan  from 'morgan'
+import cors from 'cors'
+import routerUsers from './routes/users.route.js'
+import routerLogin from './routes/login.route.js'
+
+
+
+const app = express ();
+
+app.use (morgan('dev'))
+app.use(cors());
+app.use (express.json())
+app.use (express.urlencoded({extended: false}))
+
+
+
+// handling errors
+app.use((err, req, res, next) => {
+    return res.status(500).json({
+      status: "error",
+      message: err.message,
+    });
+  });
+
+  app.use(routerUsers)
+  app.use(routerLogin)
+app.listen (pgdb.PORT)
+console.log ('conectado en el puerto', pgdb.PORT)
+
